923b5bf53848d13a492917e17267803a10e1d68f
Cara Lagumen <CaraLagumen@gmail.com>
2020-03-12 19:20:04 -0700

    installed mongodb

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
+const app = require(`./server/app`);
 const mongoose = require(`mongoose`);
 const dotenv = require(`dotenv`);
 
 dotenv.config({ path: `./config.env` });
-const app = require(`./server/app`);
-
-//CONNECT TO DB
 const DB = process.env.DATABASE.replace(
-   `<PASSWORD>`,
-   process.env.DATABASE_PASSWORD
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
 );
 
+//CONNECT TO DB
 mongoose
-   .connect(DB, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-   })
-   .then(() => console.log(`DB connection successful.`));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log(`DB connection successful.`));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
-   console.log(`App running on port ${port}...`)
+  console.log(`App running on port ${port}...`)
 );
 
 process.on(`uncaughtException`, err => {
-   console.log(`Uncaught exception. Shutting down...`);
-   console.log(err.name, err.message);
-   process.exit(1);
- });
+  console.log(`Uncaught exception. Shutting down...`);
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 process.on(`unhandledRejection`, err => {
   console.log(`Unhandled rejection. Shutting down...`);
