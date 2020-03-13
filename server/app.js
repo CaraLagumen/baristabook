7a1b91f401b74de0699bb532251888037e7de314
Cara Lagumen <CaraLagumen@gmail.com>
2020-03-12 18:59:27 -0700

    changed build path

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.options(`*`, cors());
 
 //SERVE STATIC FILES
-app.use(`/`, express.static(path.join(__dirname, `dist`)));
+app.use(`/`, express.static(path.join(__dirname, `../dist`)));
 
 //SET HTTP SECURITY HEADERS
 app.use(helmet());
